2f9ecfd6cc7a7e22b7ebb4e251ed09fba1d1a881
NatalyTorrijos <167800606+NatalyTorrijos@users.noreply.github.com>
2025-11-17 08:58:30 -0500

    Terminas nivel 3 primera parte con 3 dinamicas

---
+++ ProyectoFinal/Assets/GAME/Scripts/Scripts Nivel 3/RecogerPrismas.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CollectibleController.Instance.SpawnNextCollectible();
+            Destroy(gameObject);
+        }
+    }
+}
+
+
